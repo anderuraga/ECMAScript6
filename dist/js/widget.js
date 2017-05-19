093ab07b042bdd6341e4cd55d8ee0e9aadf96142
anderuraga <ander.uraga@gmail.com>
2017-05-19 13:32:48 +0200

    backgorund animado y mas codigo js

--- dist/js/widget.js
+++ dist/js/widget.js
@@ -6,7 +6,8 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 var COUNTRY = 'bilbao',
     API_ID = '4ee841cfbd26ba9d323591e315799f67',
-    END_POINT = 'http://api.openweathermap.org/data/2.5/weather';
+    END_POINT = 'http://api.openweathermap.org/data/2.5/weather',
+    KELVIN = -273.15;
 
 var Widget = function () {
     function Widget(country, wrapper_id) {
@@ -14,21 +15,22 @@ var Widget = function () {
 
         this.country = country;
         this.wrapper = document.getElementById(wrapper_id);
-        this.data = null; //json data
-
-        this.paint();
-        this.listenners();
-        this.callApi();
-
         console.debug('Widget:constructor ' + this.country);
+
+        this.data = null; //json data
+        var id = this.paint();
+        console.log(id);
+        this.listenners();
+        this.repaint(id);
     }
 
     _createClass(Widget, [{
         key: 'paint',
         value: function paint() {
-            var card = '<div id="widget-' + this.country + '" class="weather-card">\n                        <button id="widget-btn-' + this.country + '">x</button>\n                        <div class="weather-icon"></div>\n                        <h1>X\xBA</h1>\n                        <p>cargando...</p>\n                    </div>';
-            this.wrapper.innerHTML += card;
             console.debug('Widget:paint card');
+            var card = '<div id="widget-' + this.country + '" class="weather-card">\n                        <button id="widget-btn-' + this.country + '">x</button>\n                        <div class="weather-icon cloud"></div>\n                        <h1 id="widget-temp-' + this.country + '">X\xBA</h1>\n                        <p>cargando...</p>\n                    </div>';
+            this.wrapper.innerHTML += card;
+            return this.country;
         }
     }, {
         key: 'listenners',
@@ -40,11 +42,25 @@ var Widget = function () {
         key: 'callApi',
         value: function callApi() {
             console.debug('Widget:callApi');
+            var promesa = this.ajaxGet(END_POINT + '?q=' + this.country + '&appid=' + API_ID);
+            return promesa;
         }
     }, {
         key: 'repaint',
-        value: function repaint() {
+        value: function repaint(id) {
             console.debug('Widget:repaint');
+            var promesa = this.callApi();
+            promesa.then(function (data) {
+                var json = JSON.parse(data);
+                console.log('response openWheather %o', json);
+                var widget = document.getElementById('widget-' + id);
+                widget.querySelector('p').textContent = json.name;
+                var temp = Math.round(json.main.temp + KELVIN);
+                widget.querySelector('h1').textContent = temp + '\xBA';
+            }).catch(function (error) {
+                console.warn('openWheather Error %o', error);
+                widget.querySelector('p').textContent = 'Error';
+            });
         }
     }, {
         key: 'destroy',
@@ -55,6 +71,27 @@ var Widget = function () {
             setInterval(function () {
                 widget.remove();
             }, 1000);
+        }
+    }, {
+        key: 'ajaxGet',
+        value: function ajaxGet(url) {
+            return new Promise(function (resolve, reject) {
+                var req = new XMLHttpRequest();
+                req.open("GET", url);
+                req.onload = function () {
+                    if (req.status === 200) {
+                        resolve(req.response);
+                    } else {
+                        reject(new Error(req.statusText));
+                    }
+                };
+
+                req.onerror = function () {
+                    reject(new Error("Network error"));
+                };
+
+                req.send();
+            });
         }
     }]);
 
--- src/js/widget.js
+++ src/js/widget.js
@@ -2,54 +2,91 @@
 
 const COUNTRY = 'bilbao',
       API_ID = '4ee841cfbd26ba9d323591e315799f67',
-      END_POINT = 'http://api.openweathermap.org/data/2.5/weather';
+      END_POINT = 'http://api.openweathermap.org/data/2.5/weather',
+      KELVIN = -273.15;
 
-class Widget{
+class Widget {
 
-    constructor(country, wrapper_id ){
+    constructor(country, wrapper_id) {
         this.country = country;
         this.wrapper = document.getElementById(wrapper_id);
-        this.data = null; //json data
-
-        this.paint();
-        this.listenners();
-        this.callApi();
-
-
-
         console.debug(`Widget:constructor ${this.country}`);
+
+        this.data = null; //json data
+        var id =this.paint();
+        console.log(id);
+        this.listenners();
+        this.repaint(id);
+
     }
 
-
-    paint(){
+    paint() {
+        console.debug(`Widget:paint card`);
         let card = `<div id="widget-${this.country}" class="weather-card">
                         <button id="widget-btn-${this.country}">x</button>
-                        <div class="weather-icon"></div>
-                        <h1>Xº</h1>
+                        <div class="weather-icon cloud"></div>
+                        <h1 id="widget-temp-${this.country}">Xº</h1>
                         <p>cargando...</p>
                     </div>`;
         this.wrapper.innerHTML += card;
-        console.debug(`Widget:paint card`);
+        return this.country;
     }
 
-    listenners(){
+    listenners() {
         let button = document.getElementById(`widget-btn-${this.country}`);
         button.addEventListener('click', this.destroy);
     }
 
-    callApi(){
+    callApi() {
         console.debug('Widget:callApi');
+        var promesa = this.ajaxGet(`${END_POINT}?q=${this.country}&appid=${API_ID}`);
+        return promesa;
+
     }
 
-    repaint(){
+    repaint(id) {
         console.debug('Widget:repaint');
+        var promesa = this.callApi();
+        promesa.then(function (data) {
+            let json = JSON.parse(data);
+            console.log('response openWheather %o', json);
+            let widget = document.getElementById(`widget-${id}`);
+            widget.querySelector('p').textContent = json.name;
+            let temp = Math.round(json.main.temp + KELVIN);
+            widget.querySelector('h1').textContent = `${temp}º`;
+        }).catch(function (error) {
+            console.warn('openWheather Error %o', error);
+            widget.querySelector('p').textContent = 'Error';
+        });
     }
 
-    destroy(){
+    destroy() {
         console.debug('Widget:destroy');
         let widget = this.parentNode;
         widget.style.animation = 'disappear 1.1s';
-        setInterval(()=>{ widget.remove(); }, 1000);
+        setInterval(() => {
+            widget.remove();
+        }, 1000);
+    }
+
+    ajaxGet(url) {
+        return new Promise(function (resolve, reject) {
+            let req = new XMLHttpRequest();
+            req.open("GET", url);
+            req.onload = function () {
+                if (req.status === 200) {
+                    resolve(req.response);
+                } else {
+                    reject(new Error(req.statusText));
+                }
+            };
+
+            req.onerror = function () {
+                reject(new Error("Network error"));
+            };
+
+            req.send();
+        });
     }
 
 };
